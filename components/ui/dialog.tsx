a309ae6b9f3e370e404927974c10a1d9d09126e7
satyam1708 <pandeysatyam1708@gmail.com>
2025-10-01 19:23:48 +0530

    Deployment

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -5,7 +5,6 @@ import * as DialogPrimitive from "@radix-ui/react-dialog";
 export const Dialog = DialogPrimitive.Root;
 export const DialogTrigger = DialogPrimitive.Trigger;
 export const DialogContent = DialogPrimitive.Content;
-export const DialogHeader = DialogPrimitive.Header;
 export const DialogTitle = DialogPrimitive.Title;
 export const DialogDescription = DialogPrimitive.Description;
 export const DialogClose = DialogPrimitive.Close;
